0eaa967350e2ff20ccf42139584a92135f7b081c
rwehresmann <igoehresmann@gmail.com>
2018-08-26 15:08:20 -0300

    Configure cors and rack-attack gem
    Very basic configurations, just to introduce these gem in project.

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module OneBitTwitter
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Rack middleware for blocking & throttling abusive requests
+    config.middleware.use Rack::Attack
   end
 end

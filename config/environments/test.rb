b14cb829f8327896fdd49291114d1cbb7200605e
rwehresmann <igoehresmann@gmail.com>
2018-08-30 22:48:36 -0300

    Send deprecation warings in tests to a log file
    We have a lot because some gems we use, and that is visually annoying to read the tests printed in console.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -38,8 +38,7 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
-  # Print deprecation notices to the stderr.
-  config.active_support.deprecation = :stderr
+  config.active_support.deprecation = :log
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
